7b205349fe1b56b06e07954cd81b680647b981e6
karthik <karthik202m@gmail.com>
2024-01-18 16:28:15 +0530

    this is an webpage program in render template

--- hello_flask.py
+++ hello_flask.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
   return "Flask Working fine"
+
+@app.route("/login")
+def template():
+  return render_template("web page.html")
 
 if __name__ == "__main__":
 	app.run(debug=True)
