a3104976fdbd182b72c46eeb9d64b5cef2f93015
karthik <karthik202m@gmail.com>
2024-01-19 10:26:45 +0530

    this ia an page program

--- hello_flask.py
+++ hello_flask.py
@@ -7,8 +7,16 @@ def index():
   return "Flask Working fine"
 
 @app.route("/login")
-def template():
+def templates():
   return render_template("web page.html")
+
+@app.route("/pageone")
+def template():
+  return render_template("firstpage.html")
+
+@app.route("/pagetwo")
+def templat():
+  return render_template("secondpage.html")
 
 if __name__ == "__main__":
 	app.run(debug=True)
